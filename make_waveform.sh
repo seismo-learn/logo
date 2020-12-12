9abe19b795c03baf060008aa3f791557383d3464
Dongdong Tian <seisman.info@gmail.com>
2020-12-11 23:16:01 -0500

    Move make_waveform_2.sh to make_waveform.sh

--- make_waveform.sh
+++ make_waveform.sh
@@ -2,16 +2,27 @@
 #
 # Plot the waveform used in the logo
 #
+
+# download SAC data format in .zip bundle for the event
+# 20140403T02:43:13.110Z, -20.5709, -70.4931, 22.4, KILOMETER, 7.7, mww
+wget -r -c -np http://service.iris.edu/fdsnws/dataselect/1/query\?net\=IU\&sta\=HRV\&loc\=00\&cha\=BH2\&start\=2014-04-03T02:43:13.110\&end\=2014-04-03T03:43:13.110\&format\=sac.zip -O IU.HRV.00.BH2.SAC.zip
+
+# extract and rename SAC data
+unzip IU.HRV.00.BH2.SAC.zip
+mv IU.HRV.00.BH2.M.2014.093.024313.SAC IU.HRV.00.BH2.SAC
+
 export SAC_DISPLAY_COPYRIGHT=0
-data=onkm.e
 sac << END
-dg sub tele $data
-bp c 0.001 0.01
+r IU.HRV.00.BH2.SAC
+bp c 0.005 0.01
 div &1,depmax
-w $data
+w over
 q
 END
+
 gmt begin waveform pdf
-gmt sac onkm.e -JX8c/3c -R200/1600/-1/1 -W1p,red
+gmt sac IU.HRV.00.BH2.SAC -JX8c/3c -R1000/2400/-1/1 -W2p,black
 gmt end show
-rm onkm.e
+
+rm IU.HRV.00.BH2.SAC.zip IU.HRV.00.BH2.SAC
+
