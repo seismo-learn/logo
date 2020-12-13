630e083318d42224c9ad1e0660da8f56cd89e8a9
Dongdong Tian <seisman.info@gmail.com>
2020-12-12 21:36:09 -0500

    Logo v1.0 (#2)

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # logo
-Logos for seismo-learn orgnization
+
+seismo-learn 的 logo 由三部分组成：
+
+- 震源球
+- 地震波形
+- 汉字“学”
+
+## 预览与下载
+
+- [PDF格式](logo.pdf)
+- [PNG格式](logo.png)
+- [JPG格式](logo.jpg)
+
+## 相关脚本
+
+- [`make_beachball.sh`](make_beachball.sh): 绘制震源球
+- [`make_waveform.sh`](make_waveform.sh): 绘制 IU.HRV 台记录的南美大地震的波形
---
+++ make_waveform.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# Plot the waveform used in the logo
+#
+
+# download SAC data format in .zip bundle for the event
+# 20140403T02:43:13.110Z, -20.5709, -70.4931, 22.4, KILOMETER, 7.7, mww
+wget -r -c -np http://service.iris.edu/fdsnws/dataselect/1/query\?net\=IU\&sta\=HRV\&loc\=00\&cha\=BH2\&start\=2014-04-03T02:43:13.110\&end\=2014-04-03T03:43:13.110\&format\=sac.zip -O IU.HRV.00.BH2.SAC.zip
+
+# extract and rename SAC data
+unzip IU.HRV.00.BH2.SAC.zip
+mv IU.HRV.00.BH2.M.2014.093.024313.SAC IU.HRV.00.BH2.SAC
+
+export SAC_DISPLAY_COPYRIGHT=0
+sac << END
+r IU.HRV.00.BH2.SAC
+bp c 0.005 0.01
+div &1,depmax
+w over
+q
+END
+
+gmt begin waveform pdf
+gmt sac IU.HRV.00.BH2.SAC -JX8c/3c -R1000/2400/-1/1 -W2p,black
+gmt end show
+
+rm IU.HRV.00.BH2.SAC.zip IU.HRV.00.BH2.SAC
+
